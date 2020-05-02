252c0d8baae6bb17e37a7717cd1f198315d61084
Asiimwe Delick <delickasiimwebanton@gmail.com>
2020-05-02 22:21:46 +0300

    Create addFunctions.kt

---
+++ addFunctions.kt
@@ -0,0 +1,12 @@
+//we gonna add x and y
+fun add(): Int{
+    val x: Int = 10
+    val y: Int = 30
+    return x+y
+}
+//now we have to call a function.
+fun main(args: Array<String>){
+    println(add())
+    //The answer should be 40 after compiling.
+
+}
